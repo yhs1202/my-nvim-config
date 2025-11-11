5ee3f55bfc91e078b339f30bcdac453b0ff921c9
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-11-12 08:09:43 +0900

    Add documentation links

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,4 @@
+-- doc: https://github.com/nvim-neo-tree/neo-tree.nvim
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+-- doc: https://github.com/nvim-telescope/telescope.nvim
 -- plugins/telescope.lua:
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
