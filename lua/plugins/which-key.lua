6543cc775aed46aec0c0fbc3753128194ca11b97
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-11-19 18:09:38 +0900

    Enhance which-key.nvim configuration for improved popup display and layout

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,11 +2,43 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+
+  opts = function(_, opts)
+    -- Calculate centered position for the popup
+    local screen_h = vim.o.lines
+    local screen_w = vim.o.columns
+
+    -- Set popup width to 70% of the screen width
+    local float_w = math.floor(screen_w * 0.60)
+
+    -- Align horizontally
+    local center_col = math.floor((screen_w - float_w) / 2)
+
+    -- Align vertically
+    local center_row = math.floor(screen_h * 0.50) + 7
+
+    -- Configure window options
+    opts.win = {
+      border = "rounded",
+      title = " Keymaps ",
+      title_pos = "center",
+      row = center_row,
+      col = center_col,
+      width = float_w,
+    }
+
+    opts.layout = {
+      height = { min = 4, max = 20 },
+      width = { min = 15, max = 30 },
+      spacing = 3,
+      align = "center",
+    }
+
+    opts.triggers = { "<leader>" }
+
+    return opts
+  end,
+
   keys = {
     {
       "<leader>?",
