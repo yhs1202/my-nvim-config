c18187145d255987d750b762ee2ed18d0c7d493e
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-11-23 15:58:17 +0900

    fix key mapping for finding files in snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -54,7 +54,7 @@ return {
     -- find
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fF", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
     { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
