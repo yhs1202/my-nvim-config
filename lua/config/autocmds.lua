39979b7d2098b08b60d184a8114af57bcf13c46b
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-11-24 06:26:02 +0900

    Add autocommand to set colorscheme after lazy.nvim loads plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+
+-- Set colorscheme after lazy.nvim has loaded plugins
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    vim.cmd("colorscheme habamax")
+  end,
+})
