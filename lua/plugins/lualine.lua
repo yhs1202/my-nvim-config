3d9bfcd5caf63e70ed47f610c3e873fa2423c29d
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-11-24 07:49:59 +0900

    Add lualine plugin configuration

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,75 @@
+-- doc: https://github.com/nvim-lualine/lualine.nvim
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+
+  config = function()
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = 'ayu_mirage',
+        -- theme = 'tomorrow_night',
+        -- theme = 'material',
+
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+          refresh_time = 16, -- ~60fps
+          events = {
+            'WinEnter',
+            'BufEnter',
+            'BufWritePost',
+            'SessionLoadPost',
+            'FileChangedShellPost',
+            'VimResized',
+            'Filetype',
+            'CursorMoved',
+            'CursorMovedI',
+            'ModeChanged',
+          },
+        }
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+          'filename',
+          path = 2,  -- 0: just filename, 1: relative path, 2: absolute path
+          },
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {
+          function() 
+            return "🕒 " .. os.date('%H:%M:%S') 
+          end
+        }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    })
+  end
+}
